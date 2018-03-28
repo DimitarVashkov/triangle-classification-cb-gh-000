7cb7f78213afa84ceab872acbc2128c3b04a2e51
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-28 21:08:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,11 +2,20 @@ class Triangle
   # write code here
   def initialize(a,b,c)
     @a = a
-    @b = b 
-    @c = c 
+    @b = b
+    @c = c
   end
 
   def kind
+    if @a == @b && @b == @c
+      :equilateral
+    elsif @a == @b || @b == @c || @a == @c
+      :isosceles
+    elsif (@a + @b > @c) &&  (@b + @c > @a) && (@b + @c > @a)
+      :scalene
+    else
+      raise TriangleError
 
+    end
   end
 end
